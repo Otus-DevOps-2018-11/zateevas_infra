bb885e717bf729864ac987d2a4552103b3243c50
Alex Zateev <zateevas@yandex.ru>
2019-01-29 20:53:58 +0300

    task with 2 stars

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -21,7 +21,17 @@ resource "google_compute_instance" "app" {
   metadata {
     ssh-keys = "appuser:${file(var.public_key_path)}"
   }
+
+  provisioner "file" {
+    source      = "puma.service"
+    destination = "/tmp/puma.service"
+  }
+
+  provisioner "remote-exec" {
+    script = "deploy.sh ${var.db_address}"
+  }
 }
+
 
 resource "google_compute_address" "app_ip" {
   name = "reddit-app-ip"
